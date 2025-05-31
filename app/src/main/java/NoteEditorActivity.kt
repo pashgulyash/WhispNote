5d3071b1e6ac10114ee7e47a11469624b841d919
pashgulyash <cuctem4uk@gmail.com>
2025-05-31 15:15:09 +0300

    Update NoteEditorActivity.kt

--- app/src/main/java/NoteEditorActivity.kt
+++ app/src/main/java/NoteEditorActivity.kt
@@ -3,13 +3,29 @@ package com.pashgulyash.whispnote
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pashgulyash.whispnote.databinding.ActivityNoteEditorBinding
+import com.pashgulyash.whispnote.db.DatabaseHelper
 
 class NoteEditorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNoteEditorBinding
+    private lateinit var dbHelper: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteEditorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        dbHelper = DatabaseHelper(this)
+        
+        binding.btnSave.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val content = binding.etContent.text.toString()
+            
+            if (title.isNotEmpty()) {
+                dbHelper.addNote(title, content)
+                finish() // Закрываем экран после сохранения
+            } else {
+                binding.etTitle.error = "Введите заголовок"
+            }
+        }
     }
 }
