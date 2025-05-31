2a201d9ed1dee09e90a0878cf64b9361472c2560
pashgulyash <cuctem4uk@gmail.com>
2025-05-31 12:06:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
-    kotlin("android") version "1.8.20" apply false
+    kotlin("android") version "1.9.10" apply false
 }
 
 allprojects {
